98bb2b1f3abceab684a0509fb94d1e7663d39bc4
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2023-09-22 23:34:08 -0400

    Completed lab 1

--- lab1/timetabled/src/App.jsx
+++ lab1/timetabled/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
+import Calendar from './components/Calendar'
 
 const App = () => {
 
   return (
     <div className="App">
-      <h1>Lab1</h1>
-      <h2>Welcome</h2>
-      
+      <h1>7 Days in NYC! 🌆</h1>
+      <h2>You have one week to explore the big apple. Ready, set, GO!!!</h2>
+      <Calendar />
     </div>
   )
 }
+
+export default App
---
+++ lab1/timetabled/src/components/Calendar.jsx
@@ -0,0 +1,128 @@
+import React from "react";
+import Event from './Event'
+
+
+const Calendar = () => {
+    return (
+        <div className="Calendar">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="time">8 am</td>
+                        <Event event='Brooklyn Bridge' color='green' location='Brooklyn'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">9 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event="Time Square" color="blue" location='Manhattan'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">10 am</td>
+                        <td></td>
+                        <Event event='Central Park' color='blue' location='Manhattan' />
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">11 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event="Astoria Park" color="green" location='Queens'/>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">12 pm</td>
+                        <td></td>
+                        <td></td>
+                        <Event event="Statue of Liberty" color="pink" location='Liberty Island'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">1 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">2 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">3 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">4 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">5 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Calendar;
