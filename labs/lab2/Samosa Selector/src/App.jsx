dec7bfa44ef0b8c8afbdf6d85aa0693b94a98c92
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2023-10-07 00:46:40 -0400

    Added steps 2-4 in lab2

--- labs/lab2/Samosa Selector/src/App.jsx
+++ labs/lab2/Samosa Selector/src/App.jsx
@@ -1,21 +1,24 @@
 import './App.css';
 import { useState } from 'react';
 
+
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier);
 
   return (
     <div className="App">
       <div className='header'>
         <h1>Samosa Selector</h1>
-        <h2>Count:</h2>
-        <img className='samosa' src='https://samosaflorida.com/wp-content/uploads/2023/06/samosa-1024x771.png'/>
+        <h2>Count: {count}</h2>
+        <img src='https://samosaflorida.com/wp-content/uploads/2023/06/samosa-1024x771.png' className='samosa' onClick={updateCount} />
       </div>
     </div>
   )
 }
 
-const [ccount, setCount] = useState(0);
-
-const [multiplier, setMultiplier] = useState(1);
 
 export default App
